4290e2655606e5e671a9d813b2ec2df9b45fb1e9
gfouz <gfouz1975@gmail.com>
2022-03-08 10:25:46 +0100

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://gfouz.github.io/react-workshop/",
+  //base: "https://gfouz.github.io/react-workshop/",
+  base: "/react-workshop/"
   plugins: [react()]
 })
